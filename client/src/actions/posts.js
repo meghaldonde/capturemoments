1905e065661e025f5bc04eaf7afa71af1f6636c1
meghaldonde <meghal.donde@gmail.com>
2021-07-14 16:32:33 -0400

    refactored to add constant actions

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 import * as api from '../api';
 
 //Action Creators
@@ -22,7 +23,7 @@ export const createPost = (post) => async(dispatch) => {
         const { data } = await api.createPost(post);
 
 
-        dispatch({ type: 'CREATE', payload: data });
+        dispatch({ type: CREATE, payload: data });
     } catch (error) {
         console.log(error.message);
     }
@@ -32,7 +33,7 @@ export const updatePost = (id, post) => async(dispatch) => {
     try {
         const { data } = await api.updatePost(id, post);
 
-        dispatch({ type: 'UPDATE', payload: data });
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log(error.message);
     }
@@ -42,7 +43,7 @@ export const deletePost = (id) => async(dispatch) => {
     try {
         await api.deletePost(id);
 
-        dispatch({ type: 'DELETE', payload: id });
+        dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log(error.message);
     }
@@ -52,7 +53,7 @@ export const likePost = (id) => async(dispatch) => {
     try {
         const { data } = await api.likePost(id);
 
-        dispatch({ type: 'LIKE', payload: data });
+        dispatch({ type: LIKE, payload: data });
     } catch (error) {
         console.log(error.message);
     }
--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from "cors";
 import mongoose from "mongoose";
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
+
 const app = express();
+dotenv.config();
 app.use(cors());
 //localhost:5000/posts
+
 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -15,12 +18,10 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 
 app.use('/posts', postRoutes);
-const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.tqbny.mongodb.net/moments?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port : ${process.env.PORT}`)))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
